28dbdfc138c29199f10fa200cf9468b718723eee
RuteSantos <1160663@isep.ipp.pt>
2022-12-18 21:17:05 +0000

    fixed constructor of HSHA256 to convert the result of Sum256 to hex string

--- internal/hsha256.go
+++ internal/hsha256.go
@@ -1,19 +1,22 @@
 package internal
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 type HSHA256 string
 
-func NewHSHA256(plainText string) (HSHA256, error) {
+func NewHSHA256(plainText string) HSHA256 {
 	var h HSHA256
 
 	hashArray := []byte(plainText)
 
 	sum := sha256.Sum256(hashArray)
 
-	sumString := string(sum[:])
+	encodedString := fmt.Sprintf("%x", sum)
 
-	h = HSHA256(sumString)
+	h = HSHA256(encodedString)
 
-	return h, nil
+	return h
 }
