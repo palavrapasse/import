bfafa85c0304e19cbf032c4c307f83ec18967cc7
RuteSantos <1160663@isep.ipp.pt>
2022-12-18 13:08:34 +0000

    constructor with validations for basActor, leak, platform and user

--- internal/leak.go
+++ internal/leak.go
@@ -1,9 +1,45 @@
 package internal
 
-type Context string
+import (
+	"errors"
+	"strings"
+
+	"go.starlark.net/lib/time"
+)
+
+type DateSeconds int64 // Epoch time in Seconds
+type context string
 
 type Leak struct {
 	LeakId      AutoGenKey
-	ShareDateSC int
-	Context     Context
+	ShareDateSC DateSeconds
+	Context     context
+}
+
+func NewLeak(context string) (Leak, error) {
+	currentTime := time.Now()
+
+	l := Leak{
+		ShareDateSC: currentTime.Unix(),
+	}
+
+	err := l.SetContext(context)
+
+	return l, err
+}
+
+func (l *Leak) SetContext(c string) error {
+	size := len(strings.TrimSpace(c))
+
+	if size == 0 {
+		return errors.New("leak context can not be nil or empty")
+	}
+
+	if size > 130 {
+		return errors.New("leak context exceeds 130 characters")
+	}
+
+	l.Context = context(c)
+
+	return nil
 }
