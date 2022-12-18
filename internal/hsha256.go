4e84f1a4ac721f34bbf34749e6b9c553d845fb40
RuteSantos <1160663@isep.ipp.pt>
2022-12-18 20:53:46 +0000

    fixed constructor of HSHA256

--- internal/hsha256.go
+++ internal/hsha256.go
@@ -1,12 +1,19 @@
 package internal
 
+import "crypto/sha256"
+
 type HSHA256 string
 
-func NewHSHA256(hash string) (HSHA256, error) {
+func NewHSHA256(plainText string) (HSHA256, error) {
 	var h HSHA256
 
-	h = HSHA256(hash)
+	hashArray := []byte(plainText)
+
+	sum := sha256.Sum256(hashArray)
+
+	sumString := string(sum[:])
+
+	h = HSHA256(sumString)
 
 	return h, nil
-
 }
