513a09c28d9605647717d91e7fa2a598ffe449bc
RuteSantos <1160663@isep.ipp.pt>
2022-12-21 19:40:18 +0000

    unit tests for function linesToLeakParse

---
+++ internal/parser/plaintext_test.go
@@ -0,0 +1,55 @@
+package parser
+
+import "testing"
+
+func TestCannotParseLinesToLeakWithOnlyOneLineWhichIsInvalid(t *testing.T) {
+	lines := []string{"fghj2@aaa"}
+
+	_, err := linesToLeakParse(lines)
+
+	if err == nil {
+		t.Fatalf("Lines designated by the string below only contains one line which is invalid, but no error was identified\nString: %s", lines)
+	}
+}
+
+func TestCannotParseLinesToLeakWithMultipleLinesWhichAreInvalid(t *testing.T) {
+	lines := []string{"fghj2@aaa", "fghj2,dghf"}
+
+	_, err := linesToLeakParse(lines)
+
+	if err == nil {
+		t.Fatalf("Lines designated by the string below contains multiple lines which are invalid, but no error was identified\nString: %s", lines)
+	}
+
+	if len(lines) != len(err) {
+		t.Fatalf("Lines contains %v lines which are invalid, but an error contains %v errors. They must be the same", len(lines), len(err))
+	}
+}
+
+func TestCanParseLinesToLeakWithSomeInvalidLines(t *testing.T) {
+	lines := []string{"fghj2@aaa", "fghj2,dghf", "fghj2@aaa,dghf"}
+
+	leak, err := linesToLeakParse(lines)
+
+	if err == nil {
+		t.Fatalf("Lines designated by the string below contains some invalid lines, but no error was identified\nString: %s", lines)
+	}
+
+	if len(leak) == 0 {
+		t.Fatalf("Lines designated by the string below contains some valid lines, but Leak is empty\nString: %s", lines)
+	}
+}
+
+func TestCanParseLinesToLeakWithOnlyValidLines(t *testing.T) {
+	lines := []string{"test@aaa,dghf", "fghj2@aaa;dghf"}
+
+	leak, err := linesToLeakParse(lines)
+
+	if err != nil {
+		t.Fatalf("Lines designated by the string below does not contain invalid lines, but an error was identified\nString: %s", lines)
+	}
+
+	if len(leak) == 0 {
+		t.Fatalf("Lines designated by the string below contains some valid lines, but Leak is empty\nString: %s", lines)
+	}
+}
