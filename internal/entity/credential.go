684fde79c82aa17660deca7d968b394024106301
RuteSantos <1160663@isep.ipp.pt>
2022-12-22 22:20:28 +0000

    validate password is not empty and unit tests

--- internal/entity/credential.go
+++ internal/entity/credential.go
@@ -1,5 +1,10 @@
 package entity
 
+import (
+	"errors"
+	"strings"
+)
+
 type Password string
 
 type Credentials struct {
@@ -7,8 +12,17 @@ type Credentials struct {
 	Password Password
 }
 
-func NewPassword(password string) Password {
-	return Password(password)
+func NewPassword(password string) (Password, error) {
+	var p Password
+
+	passwordTrim := strings.TrimSpace(password)
+	err := checkIfPasswordConstraintsAreMet(passwordTrim)
+
+	if err == nil {
+		p = Password(passwordTrim)
+	}
+
+	return p, err
 }
 
 func NewCredentials(password Password) Credentials {
@@ -22,4 +36,14 @@ func (c Credentials) Copy(key AutoGenKey) Credentials {
 		CredId:   key,
 		Password: c.Password,
 	}
+}
+
+func checkIfPasswordConstraintsAreMet(p string) error {
+	size := len(p)
+
+	if size == 0 {
+		return errors.New("password can not be empty")
+	}
+
+	return nil
 }
