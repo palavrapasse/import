1cfea3ab57347fea4e91be4c2d114e6e3641eb09
RuteSantos <1160663@isep.ipp.pt>
2022-12-27 19:09:18 +0000

    add flags for path, context, platform, and share-date

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"sort"
+	"strings"
 
+	"github.com/palavrapasse/import/internal/parser"
 	"github.com/urfave/cli/v2"
 )
 
@@ -20,41 +21,59 @@ func main() {
 	// log.Println(errors)
 	// log.Println(leakParse)
 
+	var path string
+	var context string
+	var platforms string
+	var sharedate string
+
 	app := &cli.App{
 		Name:                 "import",
 		Version:              "v0.0.1",
 		Usage:                "Imports leak files into SQLite",
 		EnableBashCompletion: true,
-		Commands: []*cli.Command{
-			{
-				Name:    "leak",
-				Aliases: []string{"l"},
-				Usage:   "import leak from file",
-				Action: func(cCtx *cli.Context) error {
-					if cCtx.Args().Len() == 0 {
-						fmt.Println("Command -> missing file")
-						return nil
-					}
-
-					fmt.Println("Command -> import leak from file: ", cCtx.Args().First())
+		Commands:             []*cli.Command{},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "leak-path",
+				Aliases:  []string{"lp"},
+				Usage:    "Load leak from `FILE`",
+				Required: true,
+				Action: func(ctx *cli.Context, v string) error {
+					validateValue(v, "leak-path")
+					path = v
 					return nil
 				},
 			},
-		},
-		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:    "config",
+				Name:    "context",
 				Aliases: []string{"c"},
-				Usage:   "Load leak from `FILE`",
+				Usage:   "Context",
+				Action: func(ctx *cli.Context, v string) error {
+					validateValue(v, "context")
+					context = v
+					return nil
+				},
 			},
-		},
-		Action: func(cCtx *cli.Context) error {
-			if cCtx.IsSet("config") {
-				file := cCtx.String("config")
-				fmt.Println("Flag -> Flag: ", file)
-				fmt.Println("Flag -> import leak from file: ", file)
-			}
-			return nil
+			&cli.StringFlag{
+				Name:    "platforms",
+				Aliases: []string{"p"},
+				Usage:   "Platforms",
+				Action: func(ctx *cli.Context, v string) error {
+					validateValue(v, "platforms")
+					platforms = v
+					return nil
+				},
+			},
+			&cli.StringFlag{
+				Name:    "share-date",
+				Aliases: []string{"sd"},
+				Usage:   "Share Date",
+				Action: func(ctx *cli.Context, v string) error {
+					validateValue(v, "share-date")
+					sharedate = v
+					return nil
+				},
+			},
 		},
 	}
 	cli.VersionFlag = &cli.BoolFlag{
@@ -66,7 +85,29 @@ func main() {
 	sort.Sort(cli.FlagsByName(app.Flags))
 	sort.Sort(cli.CommandsByName(app.Commands))
 
-	if err := app.Run(os.Args); err != nil {
+	err := app.Run(os.Args)
+	if err != nil {
 		log.Fatal(err)
+	}
+
+	if path != "" {
+		log.Println(path)
+		log.Println(context)
+		log.Println(platforms)
+		log.Println(sharedate)
+
+		var parser parser.LeakParser = parser.PlainTextLeakParser{
+			FilePath: path,
+		}
+		leakParse, errors := parser.Parse()
+		log.Println(errors)
+		log.Println(leakParse)
+
+	}
+}
+
+func validateValue(value string, flag string) {
+	if strings.TrimSpace(value) == "" {
+		log.Fatal(flag + " should not be empty or white spaces")
 	}
 }
