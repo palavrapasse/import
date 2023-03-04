79cb7d2ef74bab5b6a1050410058042da5c678b8
freitzzz <1160907@isep.ipp.pt>
2023-03-04 15:25:34 +0000

    trigger form post on submit callback and show result

--- third_party/import-web/src/routes/+page.svelte
+++ third_party/import-web/src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import { ImportForm, type Form } from '@components';
+	import http from '@http';
+
+	let response: Response | Error;
 
 	function onSubmit(form: Form) {
-		console.log(form);
+		const request = http(form);
+
+		request.then((resp) => (response = resp)).catch((err) => (response = new Error(err)));
 	}
 </script>
 
@@ -11,6 +16,12 @@
 		<div class="card-body">
 			<h2 class="card-header">Import leak</h2>
 			<ImportForm on:submit={(event) => onSubmit(event.detail)} />
+
+			{#if response instanceof Response}
+				<p class="text-success text-center">Uploaded!</p>
+			{:else if response instanceof Error}
+				<p class="text-error text-center">Failed to upload! ({response})</p>
+			{/if}
 		</div>
 	</div>
 </div>
