8e6075c625eed60f8accf5ffc92db2f09ca4176c
freitzzz <1160907@isep.ipp.pt>
2023-04-10 19:59:59 +0100

    feat: implement bash script to import leaks based on a json configuration

---
+++ third_party/import-bash/run.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+script_path=$(dirname $(realpath "$0"))
+
+read leaksdb_fp notify_url < <(echo $(cat "$script_path/args.json" | jq -r '.leaksdb_fp, .notify_url'))
+
+while read -r leak_fp
+do
+    read -r context
+    read -r leakers
+    read -r platforms
+    read -r share_date
+
+    docker run \
+        --mount "type=bind,src=$leaksdb_fp,dst=$leaksdb_fp" \
+        import --database-path="$leaksdb_fp" --leak-path="$leak_fp" --context="$context" --platforms="$platforms" --share-date="$share_date" --leakers="$leakers" --notify-url="$notify_url" --skip=true
+done < <( cat "$script_path/args.json" | jq -cr '.leaks[] | (.leak_fp, .context, .leakers, .platforms, .share_date)')
